a18e831d3fc08152b6e1904b1e20410f43c63d7e
yoyoturtle <yoyoturtle@users.noreply.github.com>
2017-05-01 19:43:43 +0800

    Add files via upload

---
+++ LCD.ino
@@ -0,0 +1,25 @@
+#include <LiquidCrystal.h>
+// 定義接腳
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+void setup(){
+ lcd.begin(16, 2);
+ lcd.print("03050592,03050273"); 
+ Serial.begin(9600);
+}
+void loop()
+{
+  lcd.setCursor(0, 1);
+  lcd.print("Yoyo,Kay"); 
+
+ if (Serial.available()) {
+   
+ delay(100);
+ // 清除 LCD 螢幕
+ lcd.clear();
+ // 讀入按鍵字元
+ while (Serial.available() > 0) {
+ // 將字元顯示在 LCD 上
+ lcd.write(Serial.read());
+ }
+ }
+}
